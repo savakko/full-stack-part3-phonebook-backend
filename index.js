8e795509ecd5ec31446c309e99351d4a587cc891
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-08 14:09:53 +0300

    Add cross-origin resource sharing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, _res) => JSON.stringify(req.body))
@@ -90,7 +92,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
   
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
